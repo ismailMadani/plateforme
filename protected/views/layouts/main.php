ccd779111a2dc8870b868a10aa49a2c54f8c52f0
plateforme <madani.ismail.ki@gmail.com>
2016-03-18 14:07:34 +0000

    save

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,252 +1,190 @@
-<?php /* @var $this Controller */ ?>
 <!DOCTYPE html>
-<html>
-  <head>
+<html lang="en">
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title><?php echo CHtml::encode(Yii::app()->name); ?> &alpha;</title>
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/flag-icon/css/flag-icon.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/dist/css/sb-admin-2.css" rel="stylesheet">
+    <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Quicksand:400,300' rel='stylesheet' type='text/css'>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.css" rel="stylesheet" type="text/css" />
+    
+    <style type="text/css">.no-js #loader { display: none;  }.js #loader { display: block; position: absolute; left: 100px; top: 0; }.se-pre-con {position: fixed;left: 0px;top: 0px;width: 100%;height: 100%;z-index: 9999;background: url(img/pre/Preloader_3.gif) center no-repeat #fff;}body {font-family: 'Quicksand', sans-serif;}</style>
+    
+</head>
+<body>
+    <div class="se-pre-con"></div>
+    <script type="text/javascript">$(window).load(function() {$(".se-pre-con").fadeOut("slow");;});</script>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/ipdnsbl/index.php"><i class="fa fa-spin fa-cog"></i> <?php echo CHtml::encode(Yii::app()->name); ?> </a>
+            </div>
+            <ul class="nav navbar-top-links navbar-right">
+                <li><?php   echo Yii::app()->user->name; 
 
+                 ?></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <li><a href="#"><i class="fa fa-user fa-fw"></i> Profil</a>
+                        </li>
+                        <li><a href="#"><i class="fa fa-gear fa-fw"></i> paramètres</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li><a href="logout.php"><i class="fa fa-sign-out fa-fw"></i> Se déconnecter</a>
+                        </li>
+                    </ul>
 
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/dist/css/skins/_all-skins.min.css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <title><?php echo CHtml::encode($this->pageTitle); ?></title>
-
-  </head>
-  <body class="hold-transition skin-blue sidebar-mini">
-    <!-- Site wrapper -->
-    <div class="wrapper">
-
-      <header class="main-header">
-        <!-- Logo -->
-        <a href="<?php echo Yii::app()->request->baseUrl; ?>" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini">PLF</span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><?php echo CHtml::encode(Yii::app()->name); ?></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
-          <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <!-- Messages: style can be found in dropdown.less-->
-              
-              <!-- Notifications: style can be found in dropdown.less -->
-              
-              <!-- Tasks: style can be found in dropdown.less -->
-              
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  
-                  <span class="hidden-xs"><?php echo Yii::app()->user->name; ?></span>
-                </a>
-                <ul class="dropdown-menu">
-                  <!-- User image -->
-                  <li class="user-header">
-                    <p>
-                      <?php echo Yii::app()->user->name; ?>
-                      <small></small>
-                    </p>
-                  </li>
-                  <!-- Menu Body -->
-                  
-                  <!-- Menu Footer-->
-                  <li class="user-footer">
-                    <div class="pull-left">
-                      <a href="<?php echo Yii::app()->request->baseUrl; ?>/site/login" class="btn btn-default btn-flat">Login</a>
-                    </div>
-                    <div class="pull-right">
-                      <a href="<?php echo Yii::app()->request->baseUrl; ?>/site/logout" class="btn btn-default btn-flat">Logout</a>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-              <!-- Control Sidebar Toggle Button -->
-              
+                    <!-- /.dropdown-user -->
+                </li>
             </ul>
-          </div>
-        </nav>
-      </header>
+            <style type="text/css">img.porter-img{height: 35px;width: 35px;}img.lang-img{height: 20px;width: 20px;}</style>
+            <div class="navbar-default sidebar" role="navigation">
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="/ipdnsbl/index.php" class="active"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                        </li>
+                       
 
-      <!-- =============================================== -->
 
-      <!-- Left side column. contains the sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          
-          
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header">MAIN NAVIGATION</li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/site/index"><i class="fa fa-circle-o"></i> Dashboard</a></li>
-                
-              </ul>
-            </li>
-            
-            <li class="treeview">
-              <a href="#">
-                  <i class="fa fa-dashboard"></i> <span>Users</span> <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/user"><i class="fa fa-circle-o"></i> Users</a></li>
-                
-              </ul>
-                <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/user/create"><i class="fa fa-circle-o"></i> create</a></li>
-                
-              </ul>
-                <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/user/admin"><i class="fa fa-circle-o"></i> manage</a></li>
-                
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                  <i class="fa fa-dashboard"></i> <span>Roles</span> <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/role"><i class="fa fa-circle-o"></i> Roles</a></li>
-                
-              </ul>
-                <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/role/create"><i class="fa fa-circle-o"></i> create</a></li>
-                
-              </ul>
-                <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/role/admin"><i class="fa fa-circle-o"></i> manage</a></li>
-                
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                  <i class="fa fa-dashboard"></i> <span>Messages</span> <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/Messages"><i class="fa fa-circle-o"></i> Messages</a></li>
-                
-              </ul>
-                <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/Messages/create"><i class="fa fa-circle-o"></i> create</a></li>
-                
-              </ul>
-                <ul class="treeview-menu">
-                <li><a href="<?php echo Yii::app()->request->baseUrl; ?>/Messages/admin"><i class="fa fa-circle-o"></i> manage</a></li>
-                
-              </ul>
-            </li>
-           
-            
-            
-            
-            
-            
-            
-            
-            
-            
 
-          </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
 
-      <!-- =============================================== -->
 
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          
-          
-          	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-            
-          
-        </section>
 
-        <!-- Main content -->
-        <section class="content">
-                <div class="row">
-                  
-                    <div class="col-md-12">
+                        <li>
+                         <a href="#" class="pmg"><i class='fa fa-wrench fa-fw'></i>&nbsp;&nbsp;&nbsp;Users</a>
+                         <ul class="nav nav-second-level">
+                             <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/user"><p class="fa fa-paperclip"></p>&nbsp;&nbsp;&nbsp; Users</a>
+                            </li>
+                            <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/user/create"><p class="fa fa-envelope-o"></p>&nbsp;&nbsp;&nbsp; create</a>
+                            </li>
+                            <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/user/admin"><p class="fa fa-bell-o"></p>&nbsp;&nbsp;&nbsp; manage</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                         <a href="#" class="pmg"><i class='fa fa-wrench fa-fw'></i>&nbsp;&nbsp;&nbsp;Roles</a>
+                         <ul class="nav nav-second-level">
+                             <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/Role"><p class="fa fa-paperclip"></p>&nbsp;&nbsp;&nbsp; Roles</a>
+                            </li>
+                            <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/Role/create"><p class="fa fa-envelope-o"></p>&nbsp;&nbsp;&nbsp; create</a>
+                            </li>
+                            <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/Role/admin"><p class="fa fa-bell-o"></p>&nbsp;&nbsp;&nbsp; manage</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
+                    <li>
+                         <a href="#" class="pmg"><i class='fa fa-wrench fa-fw'></i>&nbsp;&nbsp;&nbsp;Messages</a>
+                         <ul class="nav nav-second-level">
+                             <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/Messages"><p class="fa fa-paperclip"></p>&nbsp;&nbsp;&nbsp; Messages</a>
+                            </li>
+                            <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/Messages/create"><p class="fa fa-envelope-o"></p>&nbsp;&nbsp;&nbsp; create</a>
+                            </li>
+                            <li >
+                                <a href="<?php echo Yii::app()->request->baseUrl; ?>/Messages/admin"><p class="fa fa-bell-o"></p>&nbsp;&nbsp;&nbsp; manage</a>
+                            </li>
+                            
+                        </ul>
+                    </li>
                     
-                    <?php echo $content; ?>
-
-                    </div>
-
-
-                
-
-      
-                </div>
-            
 
 
 
-          <!-- Default box -->
-         
-          
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
 
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Version</b> 1.0.1
+                </ul>
+            </div>
         </div>
-        <strong>Copyright &copy; 2016 <a href="http://google.com">Plateforme</a>.</strong> All rights reserved.
-      </footer>
+    </nav>
 
-      
-      <!-- Add the sidebar's background. This div must be placed
-           immediately after the control sidebar -->
-      <div class="control-sidebar-bg"></div>
-    </div><!-- ./wrapper -->
 
-    <!-- jQuery 2.1.4 -->
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <!-- Bootstrap 3.3.5 -->
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap/js/bootstrap.min.js"></script>
-    <!-- SlimScroll -->
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/plugins/slimScroll/jquery.slimscroll.min.js"></script>
-    <!-- FastClick -->
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/plugins/fastclick/fastclick.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/dist/js/app.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-    <script src="<?php echo Yii::app()->request->baseUrl; ?>/css/dist/js/demo.js"></script>
-  </body>
+
+<div id="page-wrapper">
+  <div class="container-fluid">
+    <div class="row">
+
+
+
+ <section class="content-header">
+          
+          
+            <?php if(isset($this->breadcrumbs)):?>
+    <?php $this->widget('zii.widgets.CBreadcrumbs', array(
+      'links'=>$this->breadcrumbs,
+    )); ?><!-- breadcrumbs -->
+  <?php endif?>
+            
+          
+        </section>
+
+
+<?php echo $content; ?>
+
+</div>
+</div>
+</div>
+
+
+
+
+
+</div>
+<!-- /#wrapper -->
+<!-- jQuery -->
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/metisMenu/dist/metisMenu.min.js"></script>
+<!-- Custom Theme JavaScript -->
+<script src="<?php echo Yii::app()->request->baseUrl; ?>/css/assets/dist/js/sb-admin-2.js"></script>
+<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
